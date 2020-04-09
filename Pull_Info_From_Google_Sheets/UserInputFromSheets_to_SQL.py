8f38175394fec79f42fbb1f68cbcbec67adb3f65
saijasthi <33167021+saijasthi@users.noreply.github.com>
2020-04-09 15:58:32 -0400

    Add files via upload

---
+++ Pull_Info_From_Google_Sheets/UserInputFromSheets_to_SQL.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import gspread 
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('capstoneuserdata-bac01be34e74.json', scope)
+
+gc = gspread.authorize(credentials)
+
+wks = gc.open('UserInfo').sheet1
+
+print(wks.get_all_records())
